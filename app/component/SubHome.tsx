930e40788e17c00fb793e9d820e949edfaf4ed81
Athulx7 <athulkrishnapr2004@gmail.com>
2025-01-25 16:38:01 +0530

    machine4

--- app/component/SubHome.tsx
+++ app/component/SubHome.tsx
@@ -12,22 +12,34 @@ export default function SubMenu() {
       };
   return (
     <>
-      <div className="relative w-full h-[9vh] flex items-center justify-center" style={{backgroundImage:"url('/dnsSub.png')",backgroundSize:"fill"}}>
+      <div
+  className="relative w-full h-[9vh] flex items-center justify-center"
+  style={{ backgroundImage: "url('/dnsSub.png')", backgroundSize: "fill" }}
+>
+  <div className="flex items-center justify-center text-white z-10 bg-black/90 w-full h-[9vh] flex-wrap gap-4 p-4">
+    <button
+      onClick={() => handleClick('food')}
+      className="bg-black px-6 py-2 border border-blue-600 hover:border-4 font-bold text-sm sm:text-base"
+    >
+      FOOD
+    </button>
 
-        <div className="flex items-center justify-center text-white z-10 bg-black/90 w-full h-[9vh]">
-         <button  onClick={() => handleClick('food')} className="bg-black px-10 py-2 border border-blue-600 hover:border-4 font-bold ">
-         FOOD
-         </button>
+    <button
+      onClick={() => handleClick('drinks')}
+      className="bg-blue-600 px-6 py-2 border border-blue-600 hover:border-4 font-bold text-sm sm:text-base"
+    >
+      DRINKS
+    </button>
 
-         <button onClick={() => handleClick('drinks')} className="bg-blue-600 px-10 py-2 border border-blue-600 hover:border-4 ms-6 font-bold">
-         DRINKS
-         </button>
+    <button
+      onClick={() => handleClick('brunch')}
+      className="bg-black px-6 py-2 border border-blue-600 hover:border-4 font-bold text-sm sm:text-base"
+    >
+      BRUNCH
+    </button>
+  </div>
+</div>
 
-         <button  onClick={() => handleClick('brunch')} className="bg-black px-10 py-2 border border-blue-600 hover:border-4 ms-6 font-bold">
-         BRUNCH
-         </button>
-        </div>
-      </div>
 
 
 
